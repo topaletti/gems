6efd28d2489f103dcb72d5768d56cd2970944bf4
Antonio Bruletti <antonio.bruletti@gmail.com>
2021-09-07 21:35:40 +0200

    add empty app with index page and template

--- gems/urls.py
+++ gems/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+import app.views
 
 urlpatterns = [
+    path('', app.views.index, name="app/index"),
     path('admin/', admin.site.urls),
 ]
