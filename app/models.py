b902eb7cf640cff28d25513cf6fcc0612bbd7e42
Antonio Bruletti <antonio.bruletti@gmail.com>
2021-09-25 09:42:45 +0200

    create company and portfolio models

--- app/models.py
+++ app/models.py
@@ -1,12 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    description = models.CharField(max_length=2000, blank=True)
+    stock_exchange = models.CharField(max_length=50, blank=True)
+    ticker = models.CharField(max_length=12, blank=True)
+    price = models.FloatField(default=0)
+    price_history = models.JSONField(blank=True)
+    currency = models.CharField(max_length=3, blank=True)
+    website = models.CharField(max_length=50, blank=True)
+    xing_profile = models.CharField(max_length=50, blank=True)
+    kununu_profile = models.CharField(max_length=50, blank=True)
+    kununu_score = models.FloatField(default=0)
+    industry = models.CharField(max_length=50, blank=True)
 
+    def __str__(self):
+        return self.name
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class Portfolio(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    companies = models.ManyToManyField(Company, through='PortfolioCompany')
 
+class PortfolioCompany(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    buy_price = models.FloatField(default=0)
+    bought_before = models.BooleanField(default=False)
+    currently_holding = models.BooleanField(default=True)
+    last_buy_date = models.DateTimeField(blank=True)
+    last_sell_date = models.DateTimeField(blank=True)
