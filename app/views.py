503a92fbd549d440311562ac9546e35081cca1a2
Antonio Bruletti <antonio.bruletti@gmail.com>
2022-03-29 22:40:40 +0200

    add challenge page

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from django.views import generic
 from .models import Company
 
 def index(request):
-    return render(request, "app/index.html")
+    return render(request, 'app/index.html')
 
 class DatabaseView(generic.ListView):
     template_name = 'app/database.html'
@@ -13,3 +13,12 @@ class DatabaseView(generic.ListView):
 
     def get_queryset(self):
         return Company.objects.order_by('name')
+
+def challenges(request):
+    number_of_days_in_month = 30
+    context = {
+        'days_of_month': range(1,number_of_days_in_month + 1),
+        'last_forecast_day': number_of_days_in_month - 3,
+        'length_of_rating_period': number_of_days_in_month - 7,
+    }        
+    return render(request, 'app/challenges.html', context)
